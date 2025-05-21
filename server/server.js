1772a7966c8b487ce081a5ce4b7639ddc4a6fe06
Abhishek Sadhu <sdinakarbabu111@gmail.com>
2025-05-21 15:29:55 +0530

    installed cors for cross platsform access

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/dbconnection');
 const adminRoutes = require('./Routes/admin');
 const app = express();
+app.use(cors());
 app.use(express.json());
 connectDB();
 const PORT = process.env.PORT || 5000;
